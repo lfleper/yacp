8d7c0b07ebed6cd5b610da6699c40faaba13249a
Lukas Fleper <lukas.fleper@jambo-gmbh.de>
2022-01-18 22:25:31 +0100

    change overview segments

---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <ion-list>
+        <ion-list-header>
+            <ion-title>Contacts</ion-title>
+        </ion-list-header>
+    </ion-list>    
+</template>
+
+<script lang="ts">
+import {Vue, Options} from 'vue-class-component'
+import {IonList, IonListHeader, IonTitle} from '@ionic/vue'
+
+@Options({
+    components: {
+        IonList,
+        IonListHeader,
+        IonTitle
+    }
+})
+export default class ContactComponent extends Vue {
+
+    filterContacts(e: CustomEvent): void {
+        console.log("filtered");
+    }
+
+}
+</script>
--- src/views/OverviewPage.vue
+++ src/views/OverviewPage.vue
@@ -1,11 +1,13 @@
 <template>
     <ion-content fullscreen>
         <ion-header translucent>
-            <ion-segment>
+            <ion-segment 
+                @ionChange="segmentChanged" 
+                v-model="activeSegment">
                 <ion-segment-button value="contacts">
                     <ion-label>contacts</ion-label>
                 </ion-segment-button>
-                <ion-segment-button value="chats">
+                <ion-segment-button value="conversations">
                     <ion-label>chats</ion-label>
                 </ion-segment-button>
             </ion-segment>
@@ -15,7 +17,8 @@
             </ion-toolbar>
         </ion-header>
 
-        <ConversationComponent ref="conversationComponent"></ConversationComponent>
+        <conversation-component v-if="activeSegment === 'conversations'" ref="conversationComponent"></conversation-component>
+        <contact-component v-if="activeSegment === 'contacts'" ref="contactComponent"></contact-component>
     </ion-content>
 </template>
 
@@ -23,6 +26,7 @@
 import {Vue, Options} from 'vue-class-component'
 import {IonContent, IonHeader, IonToolbar, IonSearchbar,IonLabel, IonSegment, IonSegmentButton} from '@ionic/vue'
 import ConversationComponent from '@/components/ConversationComponent.vue'
+import ContactComponent from '@/components/ContactComponent.vue'
 
 @Options({
     components: {
@@ -33,13 +37,19 @@ import ConversationComponent from '@/components/ConversationComponent.vue'
         IonLabel,
         IonSegment,
         IonSegmentButton,
-        ConversationComponent
+        ConversationComponent,
+        ContactComponent
     }
 })
 export default class OverviewPage extends Vue {
+    private activeSegment = 'conversations'
 
     doFilter(e: CustomEvent): void {
         (this.$refs.conversationComponent as any).filterConversations(e);
+    }
+
+    segmentChanged(e: CustomEvent): void {
+        this.activeSegment = e.detail.value
     }
 
 }
